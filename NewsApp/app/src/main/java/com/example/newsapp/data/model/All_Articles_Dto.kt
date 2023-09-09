9694cf6c38e93c6b162d34288014cee5105316d2
Vyom Singh <112750331+vyom198@users.noreply.github.com>
2023-09-09 15:44:30 +0530

    Delete NewsApp directory

--- NewsApp/app/src/main/java/com/example/newsapp/data/model/All_Articles_Dto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsapp.data.model
-
-data class All_Articles_Dto(
-    val articles: List<Article_Dto>?,
-    val status: String?,
-    val totalResults: Int?
-)
--- NewsApp/app/src/main/java/com/example/newsapp/util/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsapp.util
-
-sealed class Resource<T>(val data:T? = null , val message : String?= null) {
-    class Success<T>(data: T?) : Resource<T>(data = data)
-    class Loading<T>(message: String?) : Resource<T>()
-    class Error<T>(message: String?) : Resource<T>(message = message)
-}
